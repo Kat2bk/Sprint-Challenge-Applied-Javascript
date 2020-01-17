4452a4b0e15d1a5f8ae1e51ac603c54dab7f1bb4
unknown <Kat2bk@gmail.com>
2020-01-17 10:12:16 -0800

    made component for header

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,46 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios
+  .get(`https://lambda-times-backend.herokuapp.com/articles`)
+  .then(response => {
+    console.log(response);
+    Array.from(response.data.articles).forEach(function(item) {
+      displayCards.append(createArticles(item));
+    });
+    Array.from(response.object.entries)
+  })
+  .catch(error => {
+    console.log("error loading axios", error);
+  });
+
+const displayCards = document.querySelector(".cards-container");
+
+function createArticles(obj) {
+  const mainCard = document.createElement("div");
+  const mainHeadline = document.createElement("div");
+  const mainAuthor = document.createElement("div");
+  const ImgContainer = document.createElement("div");
+  const ImgUrl = document.createElement("img");
+  const mainName = document.createElement("span");
+
+  mainCard.append(mainHeadline);
+  mainCard.append(mainAuthor);
+  mainAuthor.append(ImgContainer);
+  ImgContainer.append(ImgUrl);
+  mainName.append(mainAuthor);
+
+  mainCard.classList.add("card");
+  mainHeadline.classList.add("headline");
+  mainAuthor.classList.add("author");
+  ImgContainer.classList.add("img-container");
+  ImgUrl.classList.add("img");
+  mainName.classList.add("span");
+
+  ImgUrl.setAttribute("src", obj.data.articles.authorPhoto);
+  mainHeadline.textContent = obj.data.articles.headline;
+  mainName.textContent = obj.data.articles.authorName;
+
+  return mainCard;
+}
